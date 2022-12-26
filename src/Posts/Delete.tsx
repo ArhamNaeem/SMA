e26e373b05cefe3deab21fce1ff91b92b73be6e8
Arham <arhamn205@gmail.com>
2022-12-26 22:52:03 +0500

    initialized working on delete post

--- src/Posts/Delete.tsx
+++ src/Posts/Delete.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { auth, db } from "../config/firebase";
+import { getDocs, query, where, collection } from "firebase/firestore";
 export default function Delete() {
-  return (
-    <div>Delete</div>
-  )
+  const currUser = auth.currentUser;
+  const [id, setID] = useState<any>("");
+  const postRef = collection(db, "posts");
+    const getPosts = query(postRef, where("userId", "==", currUser?.uid));
+  const getInfo = async () => {
+    // if (currUser) {
+    const posts = await getDocs(getPosts);
+
+    setID(posts.docs.map((doc)=>({userId: doc.data().userId , description: doc.data().description})))
+
+    // }
+  };
+  return <>
+    <button onClick={getInfo}>Click me</button>
+    {console.log(id)}
+  
+  </>;
 }
