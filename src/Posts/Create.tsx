6afa32a78a5ae38b7b230a25bf4fb89439f6194b
Arham <arhamn205@gmail.com>
2022-12-26 20:33:00 +0500

    work on create-post partially done

--- src/Posts/Create.tsx
+++ src/Posts/Create.tsx
@@ -1,46 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { db,auth } from "../config/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import {useNavigate } from "react-router-dom";
 import {
   ref,
   uploadBytes,
 } from "firebase/storage";
 import { storage } from "../config/firebase";
-// import { v4 } from 'uuid';
 export default function Create() {
   const [imageUpload, setImageUpload] = useState<File | null>(null);
   const [img, setImg] = useState("");
-  // const [text, setText] = useState("");
-  const imageRef = ref(storage, "post/");
-
+  const [description, setDescription] = useState("");
+  const [msg, setMsg] = useState<string>("");
+  const [style, setStyle] = useState("w-screen h-12");
+  const colRef = collection(db, 'posts');
+  const navigate = useNavigate();
+  const currUser = auth.currentUser;
   const displayImg = (event: any) => {
     const file = event.target.files[0];
-
-    // Check that the file is an image
     if (!file.type.startsWith("image/")) {
-      return;
-    }
-
-    // Create an object URL for the file
+        setStyle(
+          "w-screen h-12 bg-blue-300 text-xl text-center text-white p-2 font-md"
+        );
+        setMsg("Upload an image file");
+        setTimeout(() => {
+          setStyle("w-screen h-12");
+          setMsg("");
+        }, 1000);
+        return;
+      }
+    
     const objectURL = URL.createObjectURL(file);
-
-    // Update the selectedFile state with the object URL
+    setImageUpload(file);
     setImg(objectURL);
   };
 
-  const uploadImg = async () => {
-    if (!imageUpload) return;
-    const rand = crypto.randomUUID();
-    const imgUrl = "post/" + imageUpload.name + rand;
-    const imgRef = ref(storage, imgUrl);
+  const uploadPost = async () => {
+    if (!imageUpload) {
+       setStyle(
+         "w-screen h-12 bg-blue-300 text-xl text-center text-white p-2 font-md"
+       );
+       setMsg("Incomplete info");
+       setTimeout(() => {
+         setStyle("w-screen h-12");
+         setMsg("");
+       }, 1000);
+      return;
+    }
+        const rand = crypto.randomUUID();
+        const imgUrl = "post/" + imageUpload.name + rand;
+        const imgRef = ref(storage, imgUrl);
+        setStyle(
+          "w-screen h-12 bg-blue-300 text-xl text-center text-white p-2 font-md"
+        );
+        setMsg("Posting..");
+     
     await uploadBytes(imgRef, imageUpload);
-    // list(imgRef).then((res) => {
-
-    //   //TODO: get the uploaded file to be displayed
-
-    //   console.log('hi')
-    // }
+    await addDoc(colRef, {
+      userId: currUser?.uid,
+      username: currUser?.displayName,
+      url: imgUrl,
+      description: description,
+      email: currUser?.email
+    })
+  
+      setStyle(
+        "w-screen h-12 bg-blue-300 text-xl text-center text-white p-2 font-md"
+      );
+      setMsg("Posted!");
+      setTimeout(() => {
+        setStyle("w-screen h-12");
+        setMsg("");
+        navigate("/timeline");
+      }, 1000);
   };
   return (
     <>
+      <div className={style}>{msg}</div>
       <div
         className="flex flex-col justify-center items-center h-screen text-center text-blue-300 
        lg:text-4xl p-5"
@@ -51,40 +87,35 @@ export default function Create() {
           </h1>
           <div className="w-full h-9/10">
             <div className="flex h-3/4">
-              <div className="md:ml-3 w-3/5 h-full border p-2 m-2 ml-0">
-                <label htmlFor="files" className="cursor-pointer">
-                 {img?"":"Select img"}
+              <div className="md:ml-3 w-3/5 h-full   border p-2 m-2 ml-0">
+                <div className="h-full flex justify-center">
+                  <img src={img} className="h-full" />
+                </div>
+                <label
+                  htmlFor="files"
+                  className="cursor-pointer relative bottom-full"
+                >
+                  {img ? "" : "Select img"}
                 </label>
                 <input
                   className="invisible"
                   type="file"
                   id="files"
-                  onChange={
-                    // (e) => {
-                      displayImg
-                    // if (e.target.files) {
-                    //   setImageUpload(e.target.files[0]);
-                    //   //upload img
-                    // }
-                  // }
-                  }
+                  onChange={displayImg}
                 />
-                {/* //set img height */}
-                <img src={img} className="  " />
               </div>
 
               <div>
                 <textarea
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
                   className="m-2 ml-0 p-2 h-full outline-none w-full text-sm md:text-lg resize-none"
                   placeholder="Description.."
                 ></textarea>
               </div>
             </div>
-            <button
-              //remove on click from here and make img saved after selection , use this btn for sending data to db
-              onClick={uploadImg}
-              className="mt-7"
-            >
+            <button onClick={uploadPost} className="mt-7">
               Upload Post
             </button>
           </div>
--- src/Timeline-page/TimelineMain.tsx
+++ src/Timeline-page/TimelineMain.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createContext } from 'react';
 import Navbar from './Navbar';
-
+const ImgListContext = createContext(null)
 export default function Main() {
-
+  const [imgList, setImgList] = useState(null);
+  // const updateImgList = () => {
+  //   setImgList(imgList)
+  // }
   return (
     <>
-      <Navbar />
+      {/* <ImgListContext.Provider value={imgList}> */}
+        <Navbar />
+      {/* </ImgListContext.Provider> */}
     </>
   );
 }
