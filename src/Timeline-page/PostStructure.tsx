9fc02aa4f2e3c62e6a81db3af886e8e1b5a8541e
Arham <arhamn205@gmail.com>
2023-01-01 02:53:59 +0500

    adding likes for now

---
+++ src/Timeline-page/PostStructure.tsx
@@ -0,0 +1,88 @@
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { auth, db } from '../config/firebase';
+
+//create interface
+export default function PostStructure(props: any) {
+  interface Like{
+    userId: string;
+    }
+  const likesRef = collection(db, "likes");
+    const likesDoc = query(likesRef, where("postId", "==", props.postId));
+  
+  const [likes, setLikes] = useState<null | Like[]>(null);
+  // check if current logged in user has liked the post
+
+      const currUser = auth.currentUser;
+      const addLike = async (postId: string) => {
+        await addDoc(likesRef, {
+          postId: postId,
+          userId: currUser?.uid,
+        });
+    };
+    
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc);
+    setLikes(data.docs.map((doc) => ({ userId: doc.data().userId })));
+  };
+  const addLikes = async () => {
+    try {
+      await addDoc(likesRef, {
+        userId: currUser?.uid,
+        postId: props.postId
+      })
+      if (currUser) {
+        setLikes((prev) => prev ? [...prev, { userId: currUser.uid }] : [{ userId: currUser.uid }])
+      }
+    } catch (e) {
+      console.log('Error occurred')
+    }
+  }
+    const hasUserLiked = likes?.find((like) => 
+      like.userId === currUser?.uid
+    );
+    useEffect(() => {
+    getLikes()
+    }, []);
+    
+  return (
+    <React.Fragment key={props.index}>
+      <div className="w-full flex justify-center">
+        <div className="flex text-blue-300 text-center flex-col flex-wrap justify-start items-center  w-2/3 md:w-1/3 h-9/10 p-3 border border-black lg:text-4xl ">
+          <div className="w-full h-9/10">
+            <p className="text-sm">Posted by: {props.userName}</p>
+
+            <div className="flex h-4/5">
+              <div className="md:ml-3 w-3/5 h-full  border m-2 ml-0">
+                <div className="h-full drop-shadow flex justify-center">
+                  <img src={props.url} />
+                </div>
+              </div>
+              <div>
+                <p className="w-full text-blue-500 text-lg font-semibold">
+                  Description
+                </p>
+                <textarea
+                  value={props.description}
+                  readOnly
+                  className="m-2 ml-0 p-1 h-full bg-slate-50 drop-shadow flex justify-center text-blue-600 outline-none w-22 lg:w-44 text-xs lg:text-sm  flex-wrap resize-none rounded-lg"
+                />
+              </div>
+            </div>
+            <div className="mt-4 text-md  h-1/5 w-2/4">
+              <button
+                className="absolute left-[31%] md:left-[38.3%] lg:left-[37%]  transition-all hover:scale-110"
+                onClick={addLikes}
+              >
+                {(hasUserLiked) ? <>&#10084;</>:<>&#129293;</> }
+              </button>
+
+              <p className="ml-10 text-blue-500">{likes?.length}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+    // </div>
+  );
+}
--- src/Timeline-page/TimelineMain.tsx
+++ src/Timeline-page/TimelineMain.tsx
@@ -1,17 +1,55 @@
-import React, { useState } from 'react'
-import { createContext } from 'react';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { getDownloadURL, ref } from 'firebase/storage';
+import React, { useEffect, useState } from 'react'
+import { auth, db, storage } from '../config/firebase';
 import Navbar from './Navbar';
-const ImgListContext = createContext(null)
+import PostStructure from './PostStructure';
+
 export default function Main() {
-  const [imgList, setImgList] = useState(null);
-  // const updateImgList = () => {
-  //   setImgList(imgList)
-  // }
+  const currUser = auth.currentUser;
+  const [postData, setPostData] = useState<
+    Array<{ url: string; description: string; postId: string, userName: string, likesCount:number }>
+    >([]);
+  const colRef = collection(db, "posts");
+
+  const [likesCount, setLikesCount] = useState(0);
+  const getPosts = async () => {
+    try {
+      const data = await getDocs(colRef);
+      const posts = [];
+      //iterate through the data
+      for (const doc of data.docs) {
+          const imgRef = ref(storage, doc.data().url);
+          const description = doc.data().description;
+        const postId = doc.data().postId;
+        const userName = doc.data().username
+          try {
+            const url = await getDownloadURL(imgRef);
+            posts.push({ url: url, description: description, postId: postId, userName: userName, likesCount: data.docs.length });
+          } catch (e) {
+            console.log("Error has occurred ");
+          }
+      
+      }
+      setPostData(posts);
+    } catch (e) {
+      console.log("Error has occurred");
+    }
+  };
+
+
+  useEffect(() => {
+    getPosts();
+  },[])
+ 
   return (
     <>
-      {/* <ImgListContext.Provider value={imgList}> */}
-        <Navbar />
-      {/* </ImgListContext.Provider> */}
+      <Navbar />
+      <div id="main" className="p-2  h-screen flex  flex-wrap justify-center">
+        {postData.map((data, index) => (
+          <PostStructure index={index} description={data.description} postId={data.postId} url={data.url} userName={data.userName} />
+        ))}
+      </div>
     </>
   );
 }
