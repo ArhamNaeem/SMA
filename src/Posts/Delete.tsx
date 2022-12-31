97fb18b82a02ced0b3020bd13d322793232d0bcd
Arham <arhamn205@gmail.com>
2023-01-01 02:53:34 +0500

    showing posts but not deleting

--- src/Posts/Delete.tsx
+++ src/Posts/Delete.tsx
@@ -56,8 +56,7 @@ export default function Delete() {
         // <p>{data.description}</p>
         <React.Fragment key={index}>
           <div className=" flex text-blue-300  text-center flex-col flex-wrap justify-start items-center  w-2/3 md:w-1/3  h-3/5 p-2 m-2 border border-black lg:text-4xl ">
-            <p>
-            </p>
+            <p></p>
             <div className="w-full h-9/10">
               <div className="flex h-4/5">
                 <div className="md:ml-3 w-3/5 h-full  border m-2 ml-0">
@@ -66,12 +65,24 @@ export default function Delete() {
                   </div>
                 </div>
                 <div>
-                  <p className="m-2 ml-0 p-1 h-full outline-none w-full text-sm md:text-lg  resize-none">
-                    {data.description}
+                  <p className="w-full text-blue-500 text-lg font-semibold">
+                    Description
                   </p>
+                  <textarea
+                    value={data.description}
+                    readOnly
+                    className="m-2 ml-0 p-1 h-full bg-transparent flex justify-center text-blue-600 outline-none w-22 lg:w-44 text-xs lg:text-sm  flex-wrap resize-none rounded-lg"
+                  />
                 </div>
               </div>
-              <button className="mt-8" onClick={() => { deletePost(data.postId) }}>Delete Post</button>
+              <button
+                className="mt-8 md:mt-14"
+                onClick={() => {
+                  deletePost(data.postId);
+                }}
+              >
+                Delete Post
+              </button>
             </div>
           </div>
         </React.Fragment>
